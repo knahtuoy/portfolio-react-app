c361552c901fe05471bcdeaf670a52865874d0ff
onedaycrypto <85792818+onedaycrypto@users.noreply.github.com>
2022-06-17 17:09:34 +0700

    Completed footer jsx

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,59 @@
 import React from 'react'
 import './contact.css'
+import {MdOutlineEmail} from 'react-icons/md'
+import {RiMessengerLine} from 'react-icons/ri'
+import {BsWhatsapp} from 'react-icons/bs'
+import {useRef} from 'react';
+import emailjs from 'emailjs-com'
+import {useState} from 'react'
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_6nqwznk', 'template_bd8w0xq', form.current, 'CqiCuUe93taPcRJzf')
+
+    e.target.reset()
+  };
+
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineEmail class='contact__option-icon'/>
+            <h4>Email</h4>
+            <h5>contact@willtajer.xyz</h5>
+            <a href="mailto:contact@willtajer.xyz" target="_blank">Send a message</a>
+          </article>
+          <article className="contact__option">
+            <RiMessengerLine class='contact__option-icon'/>
+            <h4>Messenger</h4>
+            <h5>Will Tajer</h5>
+            <a href="https://m.me/WillTajer" target="_blank">Send a message</a>
+          </article>
+          <article className="contact__option">
+            <BsWhatsapp class='contact__option-icon'/>
+            <h4>WhatsApp</h4>
+            <h5>+18054134124</h5>
+            <a href="https://https://api.whatsapp.com/send?phone+18054134124" target="_blank">Send a message</a>
+          </article>
+        </div>
+        {/* END OF CONTACT OPTIONS */}
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" name='name' placeholder='Your Full Name' required />
+          <input type="email" name='email' placeholder='Your Email' required />
+          <textarea name="message" rows="7" placeholder='Your message' required></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
+        {/* END OF FORM INPUTS */}
+      </div>
+    </section>
   )
 }
 
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
 import './footer.css'
+import {FaFacebookF} from 'react-icons/fa'
+import {FiInstagram} from 'react-icons/fi'
+import {IoLogoTwitter} from 'react-icons/io'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className='footer__logo'>Will Tajer</a>
+
+      <ul className='permalinks'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#testimonials">Testimonials</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://facebook.com/WillTajer">< FaFacebookF /></a>
+        <a href="https://instagram.com/WillTajer"><FiInstagram /></a>
+        <a href="https://twitter.com/Knaht_Uoy"><IoLogoTwitter /></a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; WTMD 2022. All rights reserved.</small>
+      </div>
+    </footer>
   )
 }
 
